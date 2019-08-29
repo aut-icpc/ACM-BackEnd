001dae7b40a82e3f9c2f962bbedc0c8ef362ac97
mohammadjavadArdestani <mjavad.ardestani00@gmail.com>
2019-08-29 18:39:18 +0430

    contests(2005-2018) added to admin side

--- contests/admin.py
+++ contests/admin.py
@@ -5,29 +5,29 @@ from .models import ACM
 class ACMContestAdmin(admin.ModelAdmin):
     # list_display_link = ('title', )
     list_display = ('title','show_problem',
-    'show_final_ranking_onsite' ,'show_final_ranking_online', 'show_test_data','show_judge_solution' )
+    'show_final_ranking_onsite' ,'show_final_ranking_online', )
     # list_filter = ('title',)
     search_fields = [ 'title']
 
     def show_problem(self, obj):
-        return format_html("<a href='{url}'>{text}</a>", url=obj.problems , text="problems"+obj.title)
+        return format_html("<a href='{url}'>{text}</a>", url=obj.problems , text="problems")
     show_problem.short_description = "problems"
     
     def show_final_ranking_onsite(self, obj):
-        return format_html("<a href='{url}'>{text}</a>", url="//www.google.com", text="final_ranking_onsite")
-    show_final_ranking_onsite.short_description = "final_ranking_onsite"
+        return format_html("<a href='{url}'>{text}</a>", url="//www.google.com", text="ranking onsite")
+    show_final_ranking_onsite.short_description = " final ranking onsite"
    
     def show_final_ranking_online(self, obj):
-        return format_html("<a href='{url}'>{text}</a>", url="//www.google.com", text="final_ranking_online")
-    show_final_ranking_online.short_description = "final_ranking_online"
+        return format_html("<a href='{url}'>{text}</a>", url="//www.google.com", text="ranking online")
+    show_final_ranking_online.short_description = " final ranking online"
 
-    def show_test_data(self, obj):
-        return format_html("<a href='{url}'>{text}</a>", url="//www.google.com", text="test_data")
-    show_test_data.short_description = "test_data"
+    # def show_test_data(self, obj):
+    #     return format_html("<a href='{url}'>{text}</a>", url="//www.google.com", text="test_data")
+    # show_test_data.short_description = "test_data"
 
-    def show_judge_solution(self, obj):
-        return format_html("<a href='{url}'>{text}</a>", url="//www.google.com", text="judje_solution")
-    show_judge_solution.short_description = "judge_solution"
+    # def show_judge_solution(self, obj):
+    #     return format_html("<a href='{url}'>{text}</a>", url="//www.google.com", text="judje_solution")
+    # show_judge_solution.short_description = "judge_solution"
 
 
 admin.site.register(ACM, ACMContestAdmin)
--- contests/models.py
+++ contests/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class ACM (models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=500)
     problems = models.CharField(max_length=500) 
-    final_ranking_onsite = models.CharField(max_length=50)
+    final_ranking_onsite = models.CharField(max_length=500)
     final_ranking_online = models.CharField(max_length=550)
-    test_data = models.CharField(max_length=50)
-    judge_solution = models.CharField(max_length = 50)
+    # test_data = models.CharField(max_length=50)
+    # judge_solution = models.CharField(max_length = 50)
 
     def __str__(self):
         return self.title
