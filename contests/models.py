2c4d1413d39b875058ce3efdac53bd93a136065a
mohammadjavadArdestani <mjavad.ardestani00@gmail.com>
2019-09-01 14:31:22 +0430

    contest.models edited

--- contests/models.py
+++ contests/models.py
@@ -3,18 +3,6 @@ from django.contrib.postgres.fields import ArrayField
 
 
 
-class Image(models.Model):
-    src = models.CharField(max_length = 500)
-    # thumbnail = models.CharField(max_length = 500)
-    # thumbnailheight = 300   
-    # thumbnailwidth = 400
-
-    def __str__(self):
-        return self.src
-
-
-
-
 
 class ACM (models.Model):
     title = models.CharField(max_length=500)
@@ -22,7 +10,7 @@ class ACM (models.Model):
     final_ranking_onsite = models.CharField(max_length=500)
     final_ranking_online = models.CharField(max_length=550)
     poster = models.CharField(max_length= 500,blank=True, null=True)
-    images = models.ManyToManyField(Image, blank=True, null=True )
+   
 
   
     def __str__(self):
@@ -31,4 +19,14 @@ class ACM (models.Model):
     class Meta:
         verbose_name = 'CONTEST'
         verbose_name_plural = 'CONTESTS'    
+
+class Image(models.Model):
+    src = models.CharField(max_length = 500)
+    thumbnail = models.CharField(max_length = 500)
+    thumbnailheight = 300   
+    thumbnailwidth = 400
+    images = models.ForeignKey(ACM, blank=True, null=True , related_name="contest" , on_delete= models.CASCADE)
+    def __str__(self):
+        return self.src
+
 
