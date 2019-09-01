d73100ae2cef88967d4f0fdc485ccbf9776a9841
mohammadjavadArdestani <mjavad.ardestani00@gmail.com>
2019-09-01 15:29:16 +0430

    contest.models edited

--- icpcsite/settings.py
+++ icpcsite/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
-
+    'sorl.thumbnail',
     'mainsite',
     'usermanagement',
 ]
