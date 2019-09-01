994c0ecfd99682c45d6821d90370029c3e1c9780
mohammadjavadArdestani <mjavad.ardestani00@gmail.com>
2019-09-01 13:50:56 +0430

    contest.models updated

--- contests/admin.py
+++ contests/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import ACM ,image
+from .models import ACM ,Image
 
 
 class ACMContestAdmin(admin.ModelAdmin):
     # list_display_link = ('title', )
     list_display = ('title','show_problem',
-    'show_final_ranking_onsite' ,'show_final_ranking_online','images' )
+    'show_final_ranking_onsite' ,'show_final_ranking_online','show_images' )
     # list_filter = ('title',)
     search_fields = [ 'title']
-
+    
+    def show_images(self, obj):
+        return "".join(str([i.src for i in obj.images.all()]))
+    show_images.short_description = "images"
     def show_problem(self, obj):
         return format_html("<a href='{url}'>{text}</a>", url=obj.problems , text="problems")
     show_problem.short_description = "problems"
@@ -32,7 +35,7 @@ class ACMContestAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ACM, ACMContestAdmin)
-admin.site.register(image)
+admin.site.register(Image)
 
 
 # class MoviesAdmin(admin.ModelAdmin):
