b9a518b5c508800bc162b79538c1088c4d0093fa
Erfan <Erfanabedi.78@gmail.com>
2019-09-13 23:21:39 +0430

    Added thumbnailsize to zip upload, still having problems trying not to overpopulate the database.

--- contests/admin.py
+++ contests/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from .models import Contest, Gallery, Photo 
-from photologue.admin import GalleryAdmin as RawGalleryAdmin, PhotoAdmin as RawPhotoAdmin
-from photologue.models import Photo as RawPhoto
+from photologue.admin import GalleryAdmin as RawGalleryAdmin, PhotoAdmin as RawPhotoAdmin, PhotoAdminForm as RawPhotoAdminForm
+from photologue.models import Photo as RawPhoto, PhotoSize
+from photologue.forms import UploadZipForm as RawUploadZipForm
+from django.http import HttpResponseRedirect
 from django import forms
+from django.utils.translation import ungettext, ugettext_lazy as _
+from django.contrib.admin import helpers
+from django.shortcuts import render
+
 
 def unregister_photologue():
     from photologue.models import Gallery, PhotoEffect, PhotoSize, Watermark
@@ -16,10 +22,8 @@ def unregister_photologue():
 unregister_photologue()
 
 class ContestAdmin(admin.ModelAdmin):
-    # list_display_link = ('title', )
     list_display = ('year','show_problem',
     'show_final_ranking_onsite' ,'show_final_ranking_online', )
-    # list_filter = ('title',)
     search_fields = ['year']
 
     def show_problem(self, obj):
@@ -56,23 +60,60 @@ class PhotoInline(admin.StackedInline):
     can_delete = False
     exclude = ['thumbnail_url']
 
+class GalleryInline(admin.StackedInline):
+    model = Gallery
+    can_delete = False
+
+class UploadZipForm(RawUploadZipForm):
+    thumbnail_size = forms.ModelChoiceField(PhotoSize.objects.all(),
+                                     label=_('PhotoSize'),
+                                     required=True)
+
+
+#TODO: add thumbnail_size to zip mode!!
+
 class PhotoAdmin(RawPhotoAdmin):
-    # list_display = ()
     inlines = [PhotoInline, ]
+
+    def upload_zip(self, request):
+
+        context = {
+            'title': _('Upload a zip archive of photos'),
+            'app_label': self.model._meta.app_label,
+            'opts': self.model._meta,
+            'has_change_permission': self.has_change_permission(request)
+        }
+
+        # Handle form request
+        if request.method == 'POST':
+            form = UploadZipForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save(request=request)
+                return HttpResponseRedirect('..')
+        else:
+            form = UploadZipForm()
+        context['form'] = form
+        context['adminform'] = helpers.AdminForm(form,
+                                                 list([(None, {'fields': form.base_fields})]),
+                                                 {})
+        return render(request, 'admin/photologue/photo/upload_zip.html', context)
 
 admin.site.register(RawPhoto, PhotoAdmin)
 
 
-
-
-
-# class PhotoAdminForm(forms.ModelForm):
+# class PhotoAdminForm(RawPhotoAdminForm):
 #     class Meta:
 #         model = Photo
 #         fields = '__all__'
 
 # class PhotoAdmin(RawPhotoAdmin):
 #     form = PhotoAdminForm
+
+    # def upload_zip(self, request):
+    #     return self.super().upload_zip(self, request)
+
+    # def get_urls(self):
+    #     return self.super().get_urls()
 
 # admin.site.unregister(Photo)
 # admin.site.register(Photo, PhotoAdmin)
--- contests/api/serializers.py
+++ contests/api/serializers.py
@@ -1,21 +1,27 @@
 from rest_framework import serializers
 from contests.models import Gallery, Contest, Photo
-# from photologue.models import Photo
 
-class PhotoSerializer(serializers.ModelSerializer):
-    thumbnail_size = serializers.ReadOnlyField(source='get_thumbnail_size')
-    src = serializers.ReadOnlyField(source='photo.image.name')
+class PhotoSerializer(serializers.HyperlinkedModelSerializer):
+    thumbnail_height = serializers.ReadOnlyField(source='thumbnail_size.height')
+    thumbnail_width = serializers.ReadOnlyField(source='thumbnail_size.width')
+    src = serializers.ReadOnlyField(source='get_photo_src')
+    thumbnail_url = serializers.ReadOnlyField(source='get_thumbnail_url')
+    caption = serializers.ReadOnlyField(source='photo.caption')
     class Meta:
         model = Photo
-        fields = ['thumbnail_size', 'src']
+        fields = ['src', 'thumbnail_url', 'thumbnail_height', 'thumbnail_width', 'caption']
 
 
 class GallerySerializer(serializers.ModelSerializer):
-    # photos = serializers.ReadOnlyField(source='get_photos')
-    photos = PhotoSerializer(many=True)
+    # photos = PhotoSerializer(many=True)
+    photos = serializers.ListField(read_only=True, child=PhotoSerializer())
     class Meta:
         model = Gallery
         fields = ['title', 'photos']
+        depth = 1
+
+
+    
 
 class ContestSerializer(serializers.ModelSerializer):
     gallery = GallerySerializer(many=True)
@@ -24,10 +30,80 @@ class ContestSerializer(serializers.ModelSerializer):
         fields = ['year', 'problems', 'final_ranking_onsite', 'final_ranking_online', 'gallery']
 
 
+
+
+
+
+
+
+
 # class PhotoSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Photo
 #         fields = '__all__'
 
 # If i'm a carrot, what happens,
-# mate?
+# mate? People on street
+#dada
+# class PhotoField(serializers.RelatedField):
+#     def to_representation(self, value):
+#         src = '"src": %s' % value.src
+#         thumbnail_url = '"thumbnail_url": %s' % value.thumbnail_url
+#         thumbnail_height = '"thumbnail_height": %s' % value.thumbnail_height
+#         thumbnail_width = '"thumbnail_width": %s' % value.thumbnail_width
+#         caption = '"thumbnail_url": %s' % value.caption
+#         return '{%s, %s, %s, %s, %d}' % src, thumbnail_url, thumbnail_height, thumbnail_width, caption
+
+# photos = serializers.SerializerMethodField()
+
+    # @property
+    # def get_photos(self, obj):
+    #     photo = {
+    #         'src': obj.src,
+    #         'thumbnail_url': obj.thumbnail_url,
+    #         'thumbnail_height': obj.thumbnail_height,
+    #         'thumbnail_width': obj.thumbnail_width,
+    #         'caption': obj.caption
+    #     }
+    #     return photo
+        # return PhotoSerializer(many=True).data
+
+    # def get_fields(self):
+    #     '''
+    #     Override get_fields() method to pass context to other serializers of this base class.
+
+    #     If the context contains query param "omit_data" as set to true, omit the "data" field
+    #     '''
+    #     fields = super().get_fields()
+
+    #     # Cause fields with this same base class to inherit self._context
+    #     for field_name in fields:
+    #         if isinstance(fields[field_name], serializers.ListSerializer):
+    #             if isinstance(fields[field_name].child, PhotoSerializer):
+    #                 fields[field_name].child._context = self._context
+
+    #         elif isinstance(fields[field_name], PhotoSerializer):
+    #             fields[field_name]._context = self._context
+
+    #     # Check for "omit_data" in the query params and remove data field if true
+    #     if 'request' in self._context:
+    #         omit_data = self._context['request'].query_params.get('omit_data', False)
+
+    #         if omit_data and omit_data.lower() in ['true', '1']:
+    #             fields.pop('data')
+
+    #     return fields
+
+    
+    # def get_fields(self):
+    #     obj = super().get_fields()
+    #     # obj.update({
+    #     #     "mamad": "mamad"
+    #     # })
+    #     # print( obj['photos']._kwargs['child']['src'])
+    #     print(obj['photos'])
+    #     print(obj)
+    #     return obj
+
+    # def get_photos(self, obj):
+        
