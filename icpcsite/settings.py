5d816f8e3d71d21f97314656807b9d1faf04a1bb
Erfan <Erfanabedi.78@gmail.com>
2019-09-19 17:01:38 +0430

    Minor settings.py fix

--- icpcsite/settings.py
+++ icpcsite/settings.py
@@ -99,10 +99,10 @@ DATABASES = {
        
         # Local:
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('POSTGRES_DATABASE_NAME')
-        'USER': os.getenv('POSTGRES_USER')
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD')
-        'HOST': os.getenv('POSTGRES_HOST')
+        'NAME': os.getenv('POSTGRES_DATABASE_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT')
     }
 }
@@ -158,8 +158,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
