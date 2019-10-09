7683a46fd7ea051f58eae00b3df629ae15afde34
Erfan <Erfanabedi.78@gmail.com>
2019-10-09 20:43:24 +0330

    Getting from env didn't work, hard-coding it might.

--- icpcsite/settings.py
+++ icpcsite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True if os.getenv('DEBUG_MODE') == 'True' else False
 ALLOWED_HOSTS = [
     'localhost',
     os.getenv('RUNNING_HOST'),
-    os.getenv('API_HOST')
+    'api.icpc.aut.ac.ir'
 ]
 
 
