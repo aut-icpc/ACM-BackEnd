a2bd604a03eaa5ebecf2e1d56793c0eb6299c8e5
Erfan <Erfanabedi.78@gmail.com>
2019-10-11 00:54:57 +0330

    Found a bug, debug true.

--- icpcsite/settings.py
+++ icpcsite/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False if os.getenv('DEBUG_MODE') == 'True' else False
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
